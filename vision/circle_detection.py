bab3c4370db75b480bbda1508532e1f59b6693d5
Alastair Feille <me@alastair.se>
2016-11-09 19:22:08 -0600

    Update circle detection code to remove false positives

--- vision/circle_detection.py
+++ vision/circle_detection.py
@@ -5,7 +5,8 @@ img = cv2.imread('colorBalls.png',0)
 img = cv2.medianBlur(img,5)
 cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=20,minRadius=0,maxRadius=0)
+circles = 
+cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,300,param1=50,param2=20,minRadius=0,maxRadius=0)
 circles = np.uint16(np.around(circles))
 print circles[0,:]
 
